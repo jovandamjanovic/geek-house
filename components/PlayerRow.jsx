0f3942793c74445039870df3d5997587ffcbcd2e
Milan Saric <milancius@gmail.com>
2023-02-24 16:34:00 +0100

    Quality of life improvements

--- components/PlayerRow.jsx
+++ components/PlayerRow.jsx
@@ -6,51 +6,51 @@ import { Collapse } from '@mui/material';
 import PlayerPointDetails from './PlayerPointDetails';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 14,
+    },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover,
-  },
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+    },
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
 }));
 
-const PlayerRow = ({ player: { ime, poeni, poeniPoKolu }}) => {
-  const [open, setOpen] = useState(false);
+const PlayerRow = ({ player: { ime, poeni, poeniPoKolu } }) => {
+    const [open, setOpen] = useState(false);
 
-  const toggleOpen = useCallback(() => {
-    setOpen((open) => !open);
-  }, [setOpen]);
+    const toggleOpen = useCallback(() => {
+        setOpen((open) => !open);
+    }, [setOpen]);
 
-  return (
-    <>
-      <StyledTableRow
-        sx={{
-          '&:last-child td, &:last-child th': {
-            border: 0,
-          },
-        }}
-        onClick={toggleOpen}
-      >
-        <StyledTableCell component='th' scope='row'>
-          {ime}
-        </StyledTableCell>
-        <StyledTableCell align='right'>{poeni}</StyledTableCell>
-      </StyledTableRow>
-      <TableRow>
-        <TableCell colSpan={2} style={{ paddingBottom: 0, paddingTop: 0 }}>
-          <Collapse in={open} timeout='auto' unmountOnExit>
-            <PlayerPointDetails pointDetails={poeniPoKolu}/>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </>
-  );
+    return (
+        <>
+            <StyledTableRow
+                sx={{
+                    '&:last-child td, &:last-child th': {
+                        border: 0,
+                    },
+                    cursor: 'pointer',
+                }}
+                onClick={toggleOpen}>
+                <StyledTableCell component="th" scope="row">
+                    {ime}
+                </StyledTableCell>
+                <StyledTableCell align="right">{poeni}</StyledTableCell>
+            </StyledTableRow>
+            <TableRow>
+                <TableCell colSpan={2} style={{ paddingBottom: 0, paddingTop: 0 }}>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                        <PlayerPointDetails pointDetails={poeniPoKolu} />
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </>
+    );
 };
 
 export default PlayerRow;
--- pages/_app.js
+++ pages/_app.js
@@ -9,25 +9,22 @@ import Layout from '../components/Layout';
 
 const clientSideEmotionCache = createEmotionCache();
 
-function MyApp({
-  Component,
-  emotionCache = clientSideEmotionCache,
-  pageProps,
-}) {
-  return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name='viewport' content='initial-scale=1, width=device-width' />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
-    </CacheProvider>
-  );
+function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
+    return (
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+                <title>Geek House EDH Liga</title>
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
 
 export default MyApp;
