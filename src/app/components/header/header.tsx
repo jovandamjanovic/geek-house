98895ba0fca88778e969cb118944ef900de3e225
Veljko Vračarević <vracarevicveljko@gmail.com>
2025-07-29 11:34:37 +0200

    .
    Hide header on scroll down show header on scroll up

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import {useTranslations} from '../../../hooks/useTranslations';
@@ -9,9 +9,38 @@ import styles from './header.module.css';
 
 const Header: React.FC = () => {
   const t = useTranslations('Header');
+
+  // header animation
+  const navShowHide = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    let prevScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      const header = navShowHide.current;
+
+      if (!header) return;
+
+      if (currentScrollY < prevScrollY) {
+        // Scroll up - show
+        header.style.transform = 'translateY(0)';
+      } else {
+        // Scroll down - hide
+        header.style.transform = 'translateY(-120%)';
+      }
+
+      prevScrollY = currentScrollY;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   
   return (
-    <header className={styles.header}>
+    <header className={styles.header}  
+            ref={navShowHide}>
       <Image
         src="/gh_large.png"
         alt="Logo"
---
+++ src/app/test/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import Image from "next/image";
+import {useTranslations} from '../../hooks/useTranslations';
+import styles from "./page.module.css";
+
+/**
+ * Renders the home page with a logo, welcome headings, and instructions for editing the source file.
+ *
+ * Displays the site name dynamically and provides guidance for getting started with development.
+ */
+export default function Home() {
+  const t = useTranslations('HomePage');
+
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+          <Image
+          className={styles.logo}
+          src="/gh_large.png"
+          alt="Geek House logo"
+          width={180}
+          height={180}
+          priority
+        />
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+        <h1>{t('title')}</h1>
+
+        <ol>
+          <li>
+            {t('getStarted')} <code>src/app/page.tsx</code>.
+          </li>
+          <li>{t('saveChanges')}</li>
+        </ol>
+      </main>
+    </div>
+  );
+}
